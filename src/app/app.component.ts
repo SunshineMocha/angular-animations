aa6f737ece108626e263e3bf6868636b38b86735
SunshineMocha <127298037+SunshineMocha@users.noreply.github.com>
2023-06-06 12:27:31 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, HostBinding } from '@angular/core';
+import { trigger, state, style, animate, transition, animation } from '@angular/animations';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  animations:[
+    trigger('showHide', [
+      state('show', style({
+        left: '500px',
+        opacity: 1
+      })),
+      state('hide', style({
+        left: '-300px',
+        opacity: 0
+      })),
+      transition('show => hide', [
+        animate('1s')
+      ]),
+      transition('hide => show', [
+        animate('1s')
+      ]),
+    ])
+  ]
+})
+export class AppComponent {
+  title = 'angular-animations';
+
+  isShown = true;
+
+  showHide(){
+    this.isShown = !this.isShown
+  }
+}
